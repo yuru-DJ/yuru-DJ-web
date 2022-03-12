a82754be4fa58e7536621343d2a83b55e897704c
Kazuya Izumi <shikibu9419@gmail.com>
2022-03-12 10:55:45 +0900

    Add toio bluetooth sample

---
+++ src/components/CubeParams.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+import { getNewCube } from "../api/toio";
+
+let cubeLoaded = false;
+let [x, y, angle] = [-1, -1, -1]
+const onGetPositionValue = (value: [number, number, number]) => {
+  [x, y, angle] = value;
+};
+const onGetMotionValue = (value: number[]) => console.log('motion:', value);
+
+const onClick = () => {
+  getNewCube({ onGetMotionValue, onGetPositionValue }).then(() => cubeLoaded = true)
+}
+</script>
+
+{#if cubeLoaded}
+<div>added!</div>
+<div>({x}, {y}, {angle})</div>
+{:else}
+<button on:click={onClick}>add</button>
+{/if}
