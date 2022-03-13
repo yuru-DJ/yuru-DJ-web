87c2904f02edde92bc6e9f75fb67d4658a722dce
Kazuya Izumi <shikibu9419@gmail.com>
2022-03-13 20:27:46 +0900

    Refactor cube monitor

--- src/components/CubeMonitor.svelte
+++ src/components/CubeMonitor.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 
-import { getNewCube, type Cube } from "../api/toio";
+import { getNewCube } from "../api/toio";
 import { addCube, cubeParams, updateCubeParams, latestCubeParam } from "../store";
 import RadiusButton from "./RadiusButton.svelte";
 import DjSlider from "./DJSlider.svelte";
@@ -11,9 +11,10 @@ export let role: string = 'No name';
 let cubeLoaded = false;
 let index;
 let [x, y, angle] = [-1, -1, -1]; // normalized value
-
-// const near = (now: number, prev: number): boolean => Math.abs(now - prev) < 0.01;
-const near = (now: number, prev: number): boolean => now !== prev;
+const MIN_X = 100;
+const MAX_X = 400;
+const MIN_Y = 150;
+const MAX_Y = 350;
 
 const onGetPositionValue = (value: [number, number, number]) => {
   [x, y, angle] = value;
@@ -23,18 +24,17 @@ const onGetPositionValue = (value: [number, number, number]) => {
   const prevY = params.y;
   const prevAngle = params.angle;
 
-  let numStep = 10;
-  let normX = Math.floor(((x - 100) / 300) * numStep) / numStep;
-  normX = Math.min(Math.max(normX, 0.001), 1);
-  let normY = Math.floor(((y - 150) / 200) * numStep) / numStep;
-  normY = Math.min(Math.max(normY, 0.001), 1);
-  let normAngle = Math.floor(angle / 180);
+  const normX = Math.min(Math.max((x - MIN_X) / (MAX_X - MIN_X), 0.001), 1);
+  const normY = Math.min(Math.max((y - MIN_Y) / (MAX_Y - MIN_Y), 0.001), 1);
+  const normAngle = Math.floor(angle / 180);
 
+  const near = (now: number, prev: number): boolean => Math.abs(now - prev) < 0.1;
   if (near(normX, prevX) || near(normY, prevY) || near(normAngle, prevAngle)) {
     latestCubeParam.set([index, normX, normY, normAngle]);
     updateCubeParams(index, {x: normX, y: normY, angle: normAngle});
   }
 };
+
 const onGetMotionValue = (value: number[]) => console.log('motion:', value);
 
 const onClick = () => {
@@ -53,7 +53,8 @@ const onClick = () => {
 <div class="container">
   <h2 class="role-name">{role}</h2>
   <div class="connect-toio-button-wrapper">
-    <DjSlider max={360} min={0} value={x} disabled={!cubeLoaded} />
+    <DjSlider max={MAX_X} min={MIN_X} value={x} disabled={!cubeLoaded} />
+    <DjSlider max={MAX_Y} min={MIN_Y} value={y} disabled={!cubeLoaded} />
   </div>
   <RadiusButton onClick={onClick} disabled={cubeLoaded}>{cubeLoaded ? '接続済み' : 'Toioと繋ぐ'}</RadiusButton>
 </div>
